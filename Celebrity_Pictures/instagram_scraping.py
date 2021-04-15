83040040183c518b607822a5f489c7073c40e417
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-04-16 00:59:44 +0200

    Create instagram_scraping.py

---
+++ Celebrity_Pictures/instagram_scraping.py
@@ -0,0 +1,51 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import os
+import wget
+import time
+
+driver = webdriver.Chrome("D:/Utilisateurs/formation/Documents/Desktop/chromedriver.exe")
+driver.get("http://www.instagram.com")
+
+#Mot de passe et login
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+username.clear()
+username.send_keys("Rafii_projects")
+password.clear()
+password.send_keys("DataScience")
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+
+
+
+#Chercher le hastag ou le username 
+searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+searchbox.clear()
+
+keyword = str(input("Entrer un hastag ou un nom d' utilisateur"))
+searchbox.send_keys(keyword)
+time.sleep(5)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(5)
+searchbox.send_keys(Keys.ENTER)
+time.sleep(5)
+driver.execute_script("window.scrollTo(0, 4000);")
+
+#Trouver tous les images correspendante
+images = driver.find_elements_by_tag_name('img')
+images = [image.get_attribute('src') for image in images]
+images = images[:-2]
+print('Number of scraped images: ', len(images))
+
+
+#Telecharger les images 
+path = os.getcwd()
+path = os.path.join(path, keyword[1:] + "s")
+os.mkdir(path)
+counter = 0
+for image in images:
+    save_as = os.path.join(path, keyword[1:] + str(counter) + '.jpg')
+    wget.download(image, save_as)
+    counter += 1
